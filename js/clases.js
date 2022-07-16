4e09721cc0762eab1081eec60662d01bb3608f11
Nach0Zar <50642766+Nach0Zar@users.noreply.github.com>
2022-07-16 11:36:30 -0300

    infoHoverIcon

--- js/clases.js
+++ js/clases.js
@@ -209,7 +209,24 @@ export class InterfazCarrito{
         else{
             listaElementos.innerHTML = `<div class='containerElementosResumen'><div class='nombreElementoTicket'>No hay items seleccionados</div><div class='precioElementoTicket'>$0</div></div></div>`;
         }
-        subtotalTicket.innerHTML += `<div class='containerElementosResumen'><div class='nombreElementoTicket'>Sub-Total</div><div class='precioElementoTicket'>$${this.calculador.costoSubTotal()}</div></div><div class='containerElementosResumen'><div class='nombreElementoTicket'>Costo Servicio</div><div class='precioElementoTicket'>$${this.calculador.costoServicios()}</div></div>`;
+        subtotalTicket.innerHTML += `
+        <div class='containerElementosResumen'>
+            <div class='nombreElementoTicket'>Sub-Total</div>
+            <div class='precioElementoTicket'>$${this.calculador.costoSubTotal()}</div>
+        </div>
+        <div class='containerElementosResumen'>
+            <div class='nombreElementoTicket'>
+                <div class='nombreElementoTicket'>Costo Servicio</div>
+                <div id="infoicon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                    </svg>
+                </div>
+                <div id="info">Por cada articulo agregado al carrito se sumará un importe de $750. No se sumará por cantidad del mismo artículo.</div>
+            </div>
+            <div class='precioElementoTicket'>$${this.calculador.costoServicios()}</div>
+        </div>`;
         totalTicket.innerHTML += `<div class='containerElementosResumen'><div class='nombreElementoTicket'>TOTAL</div><div class='precioElementoTicket'>$${this.calculador.costoTotal()}</div></div>`;
         let estilo;
         (this.calculador.getCarritoCompras().getArticulosSeleccionados().length > 0) ? estilo = "btn-outline-dark" : estilo = "btn-secondary disabled";
